f4d47a66d14d17976d406cf9dc8b1850e933ea23
Tamás Gavrán <b0gavtam@vasvari.org>
2024-09-05 15:52:42 +0200

    ErtekKiiras metódus, metódushívás

---
+++ ParameterProject/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Paraméter
+
+static void ErtekKiiras(int a, int b)
+{
+    Console.WriteLine("Első szám értéke: {0}, második szám értéke: {1}", a, b);
+}
+
+int a = 2, b = 2;
+ErtekKiiras(a, b);
